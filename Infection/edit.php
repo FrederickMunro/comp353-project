04a9378ef836f517f13feee0e25544a2553611c7
FrederickMunro <fredmunro7@gmail.com>
2021-11-25 17:50:15 -0500

    Created and workinG

---
+++ Infection/edit.php
@@ -0,0 +1,61 @@
+<?php require_once '../database.php';
+
+$Infection = $conn->prepare('    SELECT * FROM gnc353_2.Infection 
+                                    WHERE Infection.firstName = :firstName 
+                                        AND Infection.lastName = :lastName
+                                        AND Infection.dateOfInfection = :dateOfInfection;');
+$Infection->bindParam(":firstName", $_GET['firstName']);
+$Infection->bindParam(":lastName", $_GET['lastName']);
+$Infection->bindParam(":dateOfInfection", $_GET['dateOfInfection']);
+$Infection->execute();
+$infections = $Infection->fetch(PDO::FETCH_ASSOC);
+
+
+if(isset($_POST['firstName']) && isset($_POST['lastName']) && isset($_POST['nameOfInfection']) && isset($_POST['dateOfInfection'])) {
+    $statement = $conn->prepare('   UPDATE gnc353_2.Infection 
+                                    SET Infection.firstName = :firstName,
+                                        Infection.lastName = :lastName,
+                                        Infection.nameOfInfection = :nameOfInfection,
+                                        Infection.dateOfInfection = :dateOfInfection
+                                    WHERE Infection.firstName = :firstName 
+                                        AND Infection.lastName = :lastName
+                                        AND Infection.dateOfInfection = :dateOfInfection;');
+
+    $statement->bindParam(':firstName', $_POST['firstName']);
+    $statement->bindParam(':lastName', $_POST['lastName']);
+    $statement->bindParam(':nameOfInfection', $_POST['nameOfInfection']);
+    $statement->bindParam(':dateOfInfection', $_POST['dateOfInfection']);
+
+    if($statement->execute()) {
+        header('Location: .');
+    } else {
+        header('Location: ./edit.php?firstName='.$_POST['firstName'].'&lastName='.$_POST['lastName'].'&dateOfInfection='.$_POST['dateOfInfection']);
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Infection</title>
+</head>
+<body>
+    <h1>Infection Editing</h1>
+    <form action='./edit.php' method='post'>
+        <label for='firstName'>First Name</label> <br>
+            <input type='text' name='firstName' id='firstName' value='<?= $infections['firstName'] ?? 'NULL' ?>' readonly> <br>
+        <label for='lastName'>Last Name</label> <br>
+            <input type='text' name='lastName' id='lastName' value='<?= $infections['lastName'] ?? '2010-10-10T10:10' ?>' readonly> <br>
+        <label for='nameOfInfection'>Name of Infection</label> <br>
+            <input type='text' name='nameOfInfection' id='nameOfInfection' value='<?= $infections['nameOfInfection'] ?? 'NULL' ?>'> <br>
+        <label for='dateOfInfection'>Date of Infection</label> <br>
+            <input type='date' name='dateOfInfection' id='dateOfInfection' readonly value='<?= $infections['dateOfInfection'] ?? 'NULL' ?>'> <br>
+        <button type='submit'>Submit</button> <br>
+    </form>
+    <a href='./index.php'>Back to infections list</a>
+</body>
+</html>
---
+++ Infection/index.php
@@ -0,0 +1,44 @@
+<?php require_once '../database.php';
+
+$statement = $conn->prepare('SELECT * FROM gnc353_2.Infection');
+$statement->execute();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Infections</title>
+</head>
+<body>
+    <h1>List of Infections</h1>
+    <table>
+        <thead>
+            <tr>
+                <td>First Name</td>
+                <td>Last Name</td>
+                <td>Name of Infection</td>
+                <td>Date of Infection</td>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($row = $statement->fetch(PDO::FETCH_ASSOC, PDO::FETCH_ORI_NEXT)) { ?>
+                <tr>
+                    <td><?= $row['firstName'] ?></td>
+                    <td><?= $row['lastName'] ?></td>
+                    <td><?= $row['nameOfInfection'] ?></td>
+                    <td><?= $row['dateOfInfection'] ?></td>
+                    <td>
+                        <a href='./edit.php?firstName=<?= $row['firstName'] ?>&lastName=<?= $row['lastName'] ?>&dateOfInfection=<?= $row['dateOfInfection'] ?>'>Edit</a>
+                        <a href='./delete.php?firstName=<?= $row['firstName'] ?>&lastName=<?= $row['lastName'] ?>&dateOfInfection=<?= $row['dateOfInfection'] ?>'>Delete</a>
+                    </td>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+    <a href='./create.php'>Add</a> <br>
+    <a href='../'>Return to menu</a>
+</body>
+</html>
